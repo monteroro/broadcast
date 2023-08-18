9a23c3f55ba1fb18a68f0c83ff724b7c73820dcb
monteroro <67929541+monteroro@users.noreply.github.com>
2023-08-18 17:18:53 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        music.setVolume(200)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Heart)
+            basic.showIcon(IconNames.SmallHeart)
+        }
+    }
+    if (receivedNumber == 2) {
+        music.setVolume(200)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Square)
+            basic.showIcon(IconNames.SmallSquare)
+        }
+        basic.showIcon(IconNames.No)
+    }
+    if (receivedNumber == 3) {
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(2)
